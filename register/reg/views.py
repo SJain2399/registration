371e6e9778f7b26632bdd9d07da6b288f741a46c
sjc2399 <shrutijain9923@gmail.com>
2019-02-19 06:44:38 -0500

    login added

--- register/reg/views.py
+++ register/reg/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from reg.forms import userform, userinfoform
+
+from django.contrib.auth import login,logout,authenticate
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def index(request):
     return render(request,'reg/index.html')
@@ -32,3 +37,28 @@ def signup(request):
                              {'registered':registered,
                              'userform':user_form,
                              'userinfoform':userinfo_form })
+
+
+def user_login(request):
+    if request.method=='POST': 
+        username=request.POST.get('username')   
+        password=request.POST.get('password')
+        user=authenticate(username='username',password='password')
+        
+        if user:
+            if user.is_active():
+                login(request,user)
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                return HttpResponse('ACCOUNT NOT ACTIVE')
+
+        else:
+            return HttpResponse("Invalid login")
+
+    else:
+        return render(request,'reg/login.html')
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
